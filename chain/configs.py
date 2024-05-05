9a87119033519c0153cf6ee49b58121b22232c7c
nkoech <koech.nicholas@gro-intelligence.com>
2024-05-05 19:54:49 +0300

    broken into multiline

--- chain/configs.py
+++ chain/configs.py
@@ -6,7 +6,11 @@ from utils import (
 
 
 def generate_http_header() -> typing.Dict:
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+    user_agent = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/113.0.0.0 Safari/537.36"
+    )
     origin = "https://dexscreener.com"
     return {
         "User-Agent": user_agent,
